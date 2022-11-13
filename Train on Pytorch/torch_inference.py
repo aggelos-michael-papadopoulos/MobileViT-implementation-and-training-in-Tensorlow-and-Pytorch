7d658a0eb75928f4ebf49febe0d20b0df3cced82
papadopoulos Aggelos-Michael <65830412+aggelos-michael-papadopoulos@users.noreply.github.com>
2022-11-13 21:16:38 +0200

    Add files via upload

---
+++ Train on Pytorch/torch_inference.py
@@ -0,0 +1,79 @@
+import timm
+import torch
+import numpy as np
+
+
+# place model
+model = timm.create_model('mobilevit_xxs', num_classes=257)
+
+# specifications
+gpu_or_cpu = input('GPU_or_CPU? ')
+anestis_or_aggelos_size = int(input('224 or 256 input image? '))
+if anestis_or_aggelos_size == 224:
+    image_size = 224
+
+if anestis_or_aggelos_size == 256:
+    image_size = 256
+
+print(f'results for {gpu_or_cpu} on an image {(3, image_size, image_size)} ...')
+if gpu_or_cpu=='GPU':
+    GPU = torch.device("cuda:0")
+    model.to(GPU)
+    dummy_input = torch.randn(1, 3, image_size, image_size, dtype=torch.float).to(GPU)
+    # INIT LOGGERS
+    starter, ender = torch.cuda.Event(enable_timing=True), torch.cuda.Event(enable_timing=True)
+    repetitions = 300
+    timings=np.zeros((repetitions,1))
+    #GPU-WARM-UP
+    for _ in range(10):
+        _ = model(dummy_input)
+
+    metrics = []
+    # MEASURE PERFORMANCE
+    with torch.no_grad():
+        for rep in range(repetitions):
+            torch.cuda.empty_cache()
+            starter.record()
+            _ = model(dummy_input)
+            ender.record()
+            # WAIT FOR GPU SYNC
+            torch.cuda.synchronize()
+            curr_time = starter.elapsed_time(ender)
+            timings[rep] = curr_time
+            metrics.append(curr_time)
+    mean_syn = np.sum(timings) / repetitions
+    std_syn = np.std(timings)
+
+    # inference time
+    print(f'inference time on GPU 3090: {mean_syn} msec')
+
+if gpu_or_cpu=='CPU':
+    CPU = torch.device("cpu")
+    model.to(CPU)
+    dummy_input = torch.randn(1, 3, image_size, image_size, dtype=torch.float).to(CPU)
+    # INIT LOGGERS
+    starter, ender = torch.cuda.Event(enable_timing=True), torch.cuda.Event(enable_timing=True)
+    repetitions = 300
+    timings=np.zeros((repetitions,1))
+    #GPU-WARM-UP
+    for _ in range(10):
+        _ = model(dummy_input)
+
+    metrics = []
+    # MEASURE PERFORMANCE
+    with torch.no_grad():
+        for rep in range(repetitions):
+            torch.cuda.empty_cache()
+            starter.record()
+            _ = model(dummy_input)
+            ender.record()
+            # WAIT FOR GPU SYNC
+            torch.cuda.synchronize()
+            curr_time = starter.elapsed_time(ender)
+            timings[rep] = curr_time
+            metrics.append(curr_time)
+    mean_syn = np.sum(timings) / repetitions
+    std_syn = np.std(timings)
+
+    # inference time
+    print(f'inference time on CPU: {mean_syn} msec')
